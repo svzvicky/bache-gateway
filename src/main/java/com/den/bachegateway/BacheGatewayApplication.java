e7c9486a0b05f8676149f88aabd8a9ca6341626c
Vignesh <svzvicky@gmail.com>
2024-03-04 00:59:02 +0530

    Route to user service

---
+++ src/main/java/com/den/bachegateway/BacheGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.den.bachegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacheGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacheGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/den/bachegateway/BacheGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.den.bachegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacheGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
